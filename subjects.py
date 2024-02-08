4b5517b336651181820a6c7f060da23d1153b611
Oxiclean2514 <149957643+Oxiclean2514@users.noreply.github.com>
2024-02-08 11:35:24 +0000

    Update subjects.py

--- subjects.py
+++ subjects.py
@@ -1,6 +1,6 @@
 # Initializing required arrays and variables
 Choices = []
-Subjects = ["Physics", "Chemistry", "History", "Geography", "Computer Science"]
+Subjects = ["physics", "chemistry", "history", "geography", "computer science"]
 Subject_Numbers = [0, 0, 0, 0, 0]
 Subject_Groups = []
 Physics = []
@@ -9,9 +9,9 @@ History = []
 Geography = []
 Computer_Science = []
 Unallocated = []
-maxGroupSize = 20
-minGroupSize = 10
-numberOfStudents = 60
+maxGroupSize = 5
+minGroupSize = 2
+numberOfStudents = 10
 Spare_Places = []
 
 # Allowing for input of data
@@ -36,22 +36,21 @@ for i in range(numberOfStudents):
     Choices.append([Name, First_Choice, Second_Choice])
     # Update subject numbers and add student names to appropriate subject array
     for j in [First_Choice, Second_Choice]:
-        match j:
-            case "physics":
-                Subject_Numbers[0] = Subject_Numbers[0] + 1
-                Physics.append(Name)
-            case "chemistry":
-                Subject_Numbers[1] = Subject_Numbers[1] + 1
-                Chemistry.append(Name)
-            case "history":
-                Subject_Numbers[2] = Subject_Numbers[2] + 1
-                History.append(Name)
-            case "geography":
-                Subject_Numbers[3] = Subject_Numbers[3] + 1
-                Geography.append(Name)
-            case "computer science":
-                Subject_Numbers[4] = Subject_Numbers[4] + 1
-                Computer_Science.append(Name)
+        if j == "physics":
+            Subject_Numbers[0] = Subject_Numbers[0] + 1
+            Physics.append(Name)
+        elif j == "chemistry":
+            Subject_Numbers[1] = Subject_Numbers[1] + 1
+            Chemistry.append(Name)
+        elif j == "history":
+            Subject_Numbers[2] = Subject_Numbers[2] + 1
+            History.append(Name)
+        elif j == "geography":
+            Subject_Numbers[3] = Subject_Numbers[3] + 1
+            Geography.append(Name)
+        elif j == "computer science":
+            Subject_Numbers[4] = Subject_Numbers[4] + 1
+            Computer_Science.append(Name)
 
 # Print number of students for each subject
 for h in range(5):
@@ -59,17 +58,17 @@ for h in range(5):
 
 # Identify over and under subscribed subjects by checking subject numbers array
 for i in range(5):
-    if Subject_Numbers[i] > (2*maxGroupSize)
+    if Subject_Numbers[i] > (2*maxGroupSize):
         print(Subjects[i] + " is oversubscribed and some students have been unallocated.")
     elif Subject_Numbers[i] < (minGroupSize):
-        print(Sybjects[i] + " is undersubscribed and is not available this year.")
+        print(Subjects[i] + " is undersubscribed and is not available this year.")
 
 # Iterate through arrays containing student names for each subject
 for currentSubject in [Physics, Chemistry, History, Geography, Computer_Science]:
     # If not enough students signed up, add all students to unallocated array and clear the subject array
     if len(currentSubject) < minGroupSize:
-        Unallocated = Unallocated + i
-        currentSubjecf.clear()
+        Unallocated = Unallocated.extend(currentSubject)
+        currentSubject.clear()
         currentSubject.append("Unavailable")
     # If too many students signed up, remove last name from subject array and add to unallocated
     # array until number of students is acceptable.
@@ -84,7 +83,7 @@ for j in range(5):
         print(Subjects[j] + ":")
         Students = ', '.join(Student_Groups[j][0:19])
         print("Group 1: " + Students)
-        Students = ', '.join(Students_Groups[j][20:(len(Student_Groups[j])-1)])
+        Students = ', '.join(Student_Groups[j][20:(len(Student_Groups[j])-1)])
         print("Group 2: " + Students)
     else:
         print(Subjects[j] + ":")
@@ -114,13 +113,11 @@ for h in range(5):
     print(Subjects[h] + ": " + str(Spare_Places[h]))
     totalSparePlaces = totalSparePlaces + Spare_Places[h]
 print("Total spare places: " + str(totalSparePlaces))
-print("Total unallocated places: " + str(len(Unallocated))
+print("Total unallocated places: " + str(len(Unallocated)))
 # Works out whether spare places can cover unallocated places
 if totalSparePlaces > len(Unallocated):
     print("There are enough spare places to cover the unallocated students")
 else:
     print("There are not enough spare places to cover the unallocated students")
 
-        
-    
     
