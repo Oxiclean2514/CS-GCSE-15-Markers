5c58b4250dd5e755e0ee6c8df62177ccbcd7625e
Oxiclean2514 <149957643+Oxiclean2514@users.noreply.github.com>
2024-02-26 15:44:56 +0000

    Update computers.py

--- computers.py
+++ computers.py
@@ -57,4 +57,64 @@ while True == True:
     print("Type 2 for p5")
     print("Type 3 for p7")
     choice = input()
-    break
+        if choice == "1":
+            components.append("p3")
+            computerPrice = computerPrice + component_prices["p3"]
+            valid = True
+        elif choice == "2":
+            components.append("p5")
+            computerPrice = computerPrice + component_prices["p5"]
+            valid = True
+        elif choice == "3":
+            components.append("p7")
+            computerPrice = computerPrice + component_prices["p7"]
+            valid = True
+        else:
+            print("Invalid input. Please try again.")
+    valid = False
+    while valid != True:
+        print("Please choose a RAM option:")
+        print("Type 1 for 16GB")
+        print("Type 2 for 32GB")
+        choice = input()
+        if choice == "1":
+            components.append("16GB")
+            computerPrice = computerPrice + component_prices["16GB"]
+            valid = True
+        elif choice == "2":
+            components.append("32GB")
+            computerPrice = computerPrice + component_prices["32GB"]
+            valid = True
+        else:
+            print("Invalid input. Please try again.")
+    while valid != True:
+        print("Please choose a RAM option:")
+        print("Type 1 for 16GB")
+        print("Type 2 for 32GB")
+        choice = input()
+        if choice == "1":
+            components.append("16GB")
+            computerPrice = computerPrice + component_prices["16GB"]
+            valid = True
+        elif choice == "2":
+            components.append("32GB")
+            computerPrice = computerPrice + component_prices["32GB"]
+            valid = True
+        else:
+            print("Invalid input. Please try again.")
+    while valid != True:
+        print("Please choose a Storage option:")
+        print("Type 1 for 1TB")
+        print("Type 2 for 2TB")
+        choice = input()
+        if choice == "1":
+            components.append("1TB")
+            computerPrice = computerPrice + component_prices["1TB"]
+            valid = True
+        elif choice == "2":
+            components.append("2TB")
+            computerPrice = computerPrice + component_prices["2TB"]
+            valid = True
+        else:
+            print("Invalid input. Please try again.")
+        
